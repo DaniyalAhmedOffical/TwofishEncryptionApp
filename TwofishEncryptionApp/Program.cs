695e73820bb5bb8aa6e5c3c314cf2b5df686e44c
Daniyal <Daniyal@DESKTOP-UODOQ15>
2024-07-12 01:11:42 -0700

    Add project files.

---
+++ TwofishEncryptionApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TwofishEncryptionApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string plainText = "jetFdASbM9tonlaSsNrkMw==";
+            string keyHex = "A1AF0E74BCB0BECA048443CFD0A36D6B";
+
+            // Encrypt
+           // string encryptedText = TwofishEncryption.Encrypt64(plainText, keyHex);
+           // Console.WriteLine($"Encrypted: {encryptedText}");
+
+            // Decrypt
+            string decryptedText = TwofishEncryption.Decrypt64(plainText, keyHex);
+            Console.WriteLine($"Decrypted: {decryptedText}");
+        }
+    }
+}
